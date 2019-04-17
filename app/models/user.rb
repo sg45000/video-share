40fca928a75d6f3b77c59105ec79a3e16b29792c
sg45000 <hamakyo711@gmail.com>
2019-04-17 13:23:35 +0900

    stored cookies for login-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,25 @@ class User < ApplicationRecord
     EMAIL_REGEX = /\A[\w\-.]+@[a-z\d\-]+\.[a-z]+\z/i
     validates :email, presence: true, format: {with: EMAIL_REGEX}, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 16}
-
+    attr_accessor :remenber_token
     has_secure_password
+
+    def remenber
+        self.remenber_token = User.new_token
+        self.update_attribute(:remenber_digest, User.digest(remenber_token))
+    end
+
+    def forget
+        self.update_attribute(:remenber_digest, nil)
+    end
+
+    def authenticated?(remenber_token)
+        return false if remenber_digest.nil?  
+        BCrypt::Password.new(remenber_digest) == remenber_token
+
+    end
+
+
     class<<self
         def digest(string)
             cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
